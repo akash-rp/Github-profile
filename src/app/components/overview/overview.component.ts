1b2f4efbe061229e8249162986cd563d564c1591
AKASH KASHU <kashthus@gmail.com>
2024-02-17 07:19:08 +0530

    Added overview page

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './../../services/profile-service';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { MarkdownService } from 'ngx-markdown';
+import { Repo } from '../../models/Repo';
 
 @Component({
   selector: 'app-overview',
@@ -6,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
-  constructor() {}
+  @Input() user!: string;
+  repositories!: Repo[];
+  markdownSrc = '';
 
-  ngOnInit() {}
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit() {
+    this.profileService.getReposList(this.user).subscribe({
+      next: (repos) => {
+        this.repositories = repos.body!;
+        if (this.repositories.length >= 6) {
+          this.repositories = this.repositories.slice(0, 6);
+        }
+      },
+    });
+
+    this.markdownSrc = `https://raw.githubusercontent.com/${this.user}/${this.user}/master/README.md`;
+  }
 }
